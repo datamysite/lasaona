bd2ee88f73d28f9621f9631f7a18ae07d7e4bfc8
Waseem <waseem@datamysite.com>
2025-08-20 11:37:08 +0400

    og tags

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -57,7 +57,9 @@
                 															@foreach($properties as $val)
 	                        													<div class="team-item" style="">
 																	                <div class="team-content">
-																	                    <div class="team-image">
+																	                    <div class="team-image ribbon">
+																	                    	<span class="ribbon1"><span>{{$val->name}}</span></span>
+
 																	                        <img loading="lazy" decoding="async" width="330" height="330" src="{{URL::to('public/storage/properties/'.$val->images[0]->image)}}" alt="{{$val->name}}">                        
 																	                        <div class="team-meta">
 																	                        	<a href="{{route('real-estate.detail', $val->slug)}}" target="_blank">
