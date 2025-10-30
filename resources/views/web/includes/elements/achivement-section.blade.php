a80ac7a9d07b4f1a64712d2f1dd0567b922fb601
Waseem <waseem@datamysite.com>
2025-10-30 14:16:58 +0400

    og tags

--- resources/views/web/includes/elements/achivement-section.blade.php
+++ resources/views/web/includes/elements/achivement-section.blade.php
@@ -9,10 +9,10 @@
                         <h3 class="realated-title text-center"><span>Lasa  </span>Highlights</h3>
                         <p class=" text-center">Milestones, Media, and the Moments That Matter— From Center Stage to Closing Deals</p>                       
                         <div class=" ">
-                            <div class="images-gallery-shortcode-wrap slick" data-columns="3">
+                            <div class="images-gallery-shortcode-wrap slick" data-columns="1">
                                 <div class="images-list slider-for">
                                     @foreach($updates as $val)
-                                        <div class="image-item image-col-2">
+                                        <div class="image-item slick-current image-col-2">
                                             <div class="slide-item">
                                                 <a href="{{$val->link}}" target="_blank">
                                                     <div class="video-related" bis_skin_checked="1">
